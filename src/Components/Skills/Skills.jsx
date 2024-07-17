456b0d8d70de84edecfd8289551fac7a6dc5f8b4
agupta55 <agupta55544@gmail.com>
2024-07-17 18:10:08 +0530

    changes in desing2

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import "./Skills.css";
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h1>
+        Professional <b>Skills</b>
+      </h1>
+    </div>
+  );
+};
+
+export default Skills;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../Components/Banner/Banner";
 import Navbar from "../../Components/Navbar/Navbar";
+import Skills from "../../Components/Skills/Skills";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Skills />
     </div>
   );
 };
